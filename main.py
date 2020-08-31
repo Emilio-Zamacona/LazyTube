37b313fa83d4bac4a028d0725105a591491a85c8
Emilio-Zamacona <61280821+Emilio-Zamacona@users.noreply.github.com>
2020-08-31 15:16:15 -0300

    8/31/2020 ffmpeg txt parser
    added ffmpeg txt file parsing capability. Working on image brightness detection in order to add automatic scene deletion.

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import os
+import time
 
-path = input("que video querés editar?")
-file = path.split("\\")[-1]
+path = "E:/GitHub/LazyTube/prueba.mp4"
+file = path.split("/")[-1]
+directory = path.rsplit("/",1)[0]
 print(file)
-threshold = -45
+threshold = -30
 duration = 0.5
+silences = []
 
-os.chdir(path)
+os.chdir(directory)
+print(directory)
 os.system("ffmpeg -i "+path+" -af silencedetect=n="+str(threshold)+"dB:d="+str(duration)+" -f null - 2> ok.txt")
+
+txt_output = open("ok.txt","r")
+"""for i in txt_output.readlines():
+	if "silence" in i:
+		print(i)"""
+for i in txt_output.readlines():
+	if "silence" in i:
+		if "|" in i:
+			print((i.split(": ")[-2]).split(" ")[0])
+		else:
+			print(i.split(": ")[-1])
 input()
---
+++ test.py
@@ -0,0 +1,5 @@
+from PIL import Image
+
+wea = Image.open("C:/Users/emili/Desktop/clase 21-8 jardín/Pregunta.png")
+
+zalo = Image.open("C:/Users/emili/Desktop/clase 21-8 jardín/Pregunta.png")
